7ce7c61f3dd8b6d747ade51777377ec1f8ae589d
garretuk <mclawicanadauk@hotmail.com>
2020-02-25 18:12:55 +0100

    modification on articles and add comments

--- views/viewAllArticles.php
+++ views/viewAllArticles.php
@@ -37,7 +37,7 @@ while ($data = $posts->fetch())
         <p>
             <?= nl2br(htmlspecialchars($data['content'])) ?>
             <br />
-            <em><a href="index.php?action=article&id=<?= $data['id'] ?>">Commentaires</a></em>
+            <em><a href="index.php?action=article&id=<?= $data['id'] ?>">Consulter ce chapitre</a></em>
         </p>
     </div>
 <?php
--- views/viewPageArticle.php
+++ views/viewPageArticle.php
@@ -1,4 +1,7 @@
-<?php ob_start(); ?>
+<?php 
+
+session_start();
+ob_start(); ?>
 
 
 <section id="articles">
@@ -20,6 +23,39 @@
                     <button><a href="index.php?action=pageEdit&id=<?=$article['id']?>">Modifier</a></button>
                     <button><a href="index.php?action=delectArticle&id=<?=$article['id']?>">Supprimer</a></button>
              </div>
+         </div>
+         <div id='comments'>       <!--use while to fetch-->
+         <?php
+             
+                      while($commentsData = $comments->fetch()){        
+         ?>
+                       <div id='comment'>
+                            <h2 id='titleComment'>
+                             Publié par <strong><?= htmlspecialchars($commentsData['author']);?></strong> Le <?= $commentsData['comments_date_fr']; ?>  
+                            </h2>
+                            <p id='contentComment'>
+                              <?= nl2br( htmlspecialchars( $commentsData['comments'])); ?>
+                            </p>
+                       </div>
+                  
+        <?php
+                       
+                       }
+                       $comments->closeCursor();
+                  
+              
+               
+       ?>
+          
+                     <div id='addComments'>
+                          <form action="index.php?action=addComments&id=<?= $article['id'];?>" method='post'>
+                          <label for="comments"> <?= $_SESSION['pseudo'];?>  votre opinion est important pour nous!</label></br>
+                          <textarea name="comments" id="loginComments" cols="30" rows="10"></textarea>
+                          <input type="submit" id="bComment">
+                          
+                          </form>
+                     </div>
+         
          </div>
 
       </section>
